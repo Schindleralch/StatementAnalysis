9e11e62c6cf199e9f7b699385c39d2229e13942d
schindleralch <schindleralch@gmail.com>
2022-09-28 13:47:09 -0400

    Added Basic Binary Search Algorithm
    Will need to implement this into the statement class later on to search
    for existing statements.

---
+++ Search.py
@@ -0,0 +1,29 @@
+testlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+x = 1
+
+
+
+def BinarySearch(arr, x):
+    low = 0
+    high = len(arr)-1
+    
+    while low <= high:
+        mid = (low + high) // 2  #int division, no floats
+        if arr[mid] == x:
+            return True
+
+        elif arr[mid] > x:
+            high = (mid - 1)
+
+        elif arr[mid] < x:
+            low = mid + 1
+
+      
+    return False 
+
+result = BinarySearch(testlist, x)
+
+if (not(result)):
+   print(f"{x} not found in list")
+else: 
+    print(f"{x} found in list")
